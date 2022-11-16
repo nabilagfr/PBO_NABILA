d1727cb623d99a7f39c4313f3f368117a7315ce6
Nabila Gafernanda <114200666+nabilagfr@users.noreply.github.com>
2022-11-16 13:16:03 +0700

    Add files via upload

---
+++ nabila_12092022/DataDiri.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nabila_12092022;
+
+/**
+ *
+ * @author LABMM07
+ */
+public class DataDiri {
+    public static void main(String[] args) {
+        System.out.println("Nama    : Nabila Gafernanda\n"
+                + "No Bp   : 2101092011\n"
+                + "Prodi   : Manajemen Informatika\n"
+                + "Jurusan : Teknologi Informasi\n"
+                + "Alamat  : Jl. Seberang Palinggam\n");
+    }
+}
---
+++ nabila_12092022/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nabila_12092022;
+
+/**
+ *
+ * @author LABMM07
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World\n"+"Pemrograman Berbasis Objek");
+        System.out.println("Pemrograman Berbasis Objek");
+        //System.out.println("Pemrograman Berbasis Objek");
+    }
+}
