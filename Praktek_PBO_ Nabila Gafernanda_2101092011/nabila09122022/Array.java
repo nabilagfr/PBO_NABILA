6d0360954ba95c6f38cd431c83b8425414af86a6
Nabila Gafernanda <114200666+nabilagfr@users.noreply.github.com>
2023-01-08 20:17:10 +0700

    Update Array.java

--- Praktek_PBO_ Nabila Gafernanda_2101092011/nabila09122022/Array.java
+++ Praktek_PBO_ Nabila Gafernanda_2101092011/nabila09122022/Array.java
@@ -11,6 +11,9 @@ package nabila09122022;
  */
 public class Array {
     public static void main(String[] args) {
-        
+        int nilai[]=new int[20];
+        nilai[0] = 70;
+        System.out.println("Nilai Ke 1 "+nilai[0]); 
+        boolean results[] = (true,false);
     }
 }
