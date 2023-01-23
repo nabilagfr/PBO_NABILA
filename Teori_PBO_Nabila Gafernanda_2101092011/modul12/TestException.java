254a11d1c07413c9a26f8b8a6a9b270964e270df
Nabila Gafernanda <114200666+nabilagfr@users.noreply.github.com>
2023-01-23 22:32:17 +0700

    PBO_Teori

---
+++ Teori_PBO_Nabila Gafernanda_2101092011/modul12/TestException.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul12;
+
+/**
+ *
+ * @author User
+ */
+public class TestException {
+    public static void main(String[] args){
+        try{
+            for( int i=0; true; i++){
+                System.out.println("args["+i+"]="+ args[i]);
+            }
+        }
+        catch(ArrayIndexOutOfBoundsException ex){
+            System.out.println("Exception caught: \n\t java.lang.ArrayIndexOutOfBoundsException :"+ex.getMessage());
+        }
+        finally{
+            System.out.println("Quiting...");
+        }
+    }
+}
